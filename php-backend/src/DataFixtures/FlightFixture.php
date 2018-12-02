cbc09af408b255bdf31f95e1c28d8df2294f6f38
keei <jozef.vanicky@gmail.com>
2018-12-02 15:27:26 +0100

    FlightFixture

--- php-backend/src/DataFixtures/FlightFixture.php
+++ php-backend/src/DataFixtures/FlightFixture.php
@@ -18,14 +18,59 @@ class FlightFixture extends Fixture implements DependentFixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        $flight = new Flight();
-        $flight->setAirplane($this->getReference("B747-1"));
-        $flight->setDateOfDeparture(new \DateTime());
-        $flight->setDestination("Prague");
-        $flight->setFlightLength(new \DateTime("11:30:00"));
-        $flight->setGate($this->getReference("G1"));
-        $flight->setTimeOfDeparture(new \DateTime('12:00:00'));
-        $manager->persist($flight);
+        $flight1 = new Flight();
+        $flight1->setAirplane($this->getReference("B747-1"));
+        $flight1->setDateOfDeparture(new \DateTime());
+        $flight1->setDestination("Prague: PRG");
+        $flight1->setFlightLength(new \DateTime("01:15:00"));
+        $flight1->setGate($this->getReference("G1"));
+        $flight1->setTimeOfDeparture(new \DateTime('12:00:00'));
+        $manager->persist($flight1);
+
+        $flight2 = new Flight();
+        $flight2->setAirplane($this->getReference("B747-2"));
+        $flight2->setDateOfDeparture(new \DateTime());
+        $flight2->setDestination("Moscow: SVO");
+        $flight2->setFlightLength(new \DateTime("01:50:00"));
+        $flight2->setGate($this->getReference("G3"));
+        $flight2->setTimeOfDeparture(new \DateTime('12:10:00'));
+        $manager->persist($flight2);
+
+        $flight3 = new Flight();
+        $flight3->setAirplane($this->getReference("B737-1"));
+        $flight3->setDateOfDeparture(new \DateTime());
+        $flight3->setDestination("Bergamo: BGY");
+        $flight3->setFlightLength(new \DateTime("01:40:00"));
+        $flight3->setGate($this->getReference("G4"));
+        $flight3->setTimeOfDeparture(new \DateTime('13:53:00'));
+        $manager->persist($flight3);
+
+        $flight4 = new Flight();
+        $flight4->setAirplane($this->getReference("A320-3"));
+        $flight4->setDateOfDeparture(new \DateTime());
+        $flight4->setDestination("Reykjavík: KEF");
+        $flight4->setFlightLength(new \DateTime("03:00:00"));
+        $flight4->setGate($this->getReference("G5"));
+        $flight4->setTimeOfDeparture(new \DateTime('15:08:00'));
+        $manager->persist($flight4);
+
+        $flight5 = new Flight();
+        $flight5->setAirplane($this->getReference("A380-1"));
+        $flight5->setDateOfDeparture(new \DateTime());
+        $flight5->setDestination("New York: JFK");
+        $flight5->setFlightLength(new \DateTime("05:45:00"));
+        $flight5->setGate($this->getReference("G2"));
+        $flight5->setTimeOfDeparture(new \DateTime('15:15:00'));
+        $manager->persist($flight5);
+
+        $flight6 = new Flight();
+        $flight6->setAirplane($this->getReference("A320-1"));
+        $flight6->setDateOfDeparture(new \DateTime());
+        $flight6->setDestination("Budapest: BUD");
+        $flight6->setFlightLength(new \DateTime("01:10:00"));
+        $flight6->setGate($this->getReference("G1"));
+        $flight6->setTimeOfDeparture(new \DateTime('19:19:00'));
+        $manager->persist($flight6);
 
         $manager->flush();
     }
